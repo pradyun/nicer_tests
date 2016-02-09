561a9402240541ef320c6a0592a40b4af3e2cc56
Pradyun S. Gedam <pradyunsg@gmail.com>
2016-02-09 18:10:57 +0530

    :package: v0.1.0

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ setup(
     name="nicer-tests",
     version="0.1.0",
     description="A bunch of routines I wrote to make testing a nicer experience",
+
     author="Pradyun S. Gedam",
+
     author_email="pradyunsg@gmail.com",
     install_requires=["nose", "PyYAML"],
     packages=find_packages(),
+
     classifiers = [
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Testing",
-    ]
+    ],
+    url="http://github.com/pradyunsg/nicer-tests",
 )
